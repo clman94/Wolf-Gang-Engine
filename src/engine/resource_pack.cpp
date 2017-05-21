f2304692498812642949477c76b32f3c3f974f37
Michael Thompson <capnsword@gmail.com>
2017-05-21 12:41:42 -0700

    Possible fix to pack generation issue

--- src/engine/resource_pack.cpp
+++ src/engine/resource_pack.cpp
@@ -26,10 +26,10 @@ inline T read_unsignedint_binary(std::istream& pStream)
 template<typename T>
 inline bool write_unsignedint_binary(std::ostream& pStream, const T pVal)
 {
-	char bytes[sizeof(T)];
+	uint8_t bytes[sizeof(T)];
 	for (size_t i = 0; i < sizeof(T); i++)
-		bytes[i] = (pVal & (0xFF << (8 * i))) >> (8 * i);
-	pStream.write(bytes, sizeof(T));
+		bytes[i] = static_cast<uint8_t>((pVal & (0xFF << (8 * i))) >> (8 * i));
+	pStream.write((char*)&bytes, sizeof(T));
 	return pStream.good();
 }
 
