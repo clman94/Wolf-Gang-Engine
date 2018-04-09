0172e60b74df3d271342db4d82f2c89b4b865a9f
Michael Thompson <capnsword@gmail.com>
2018-04-08 20:53:54 -0700

    Added some fancy hash functions

---
+++ include/engine/hash.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <string>
+
+namespace hash
+{
+
+typedef std::uint32_t hash32_t;
+
+hash32_t FNV1a_32(const std::uint8_t* pData, std::size_t pSize);
+hash32_t FNV1a_32(const char* pData, int pLength = -1); // String is null terminated if length < 0
+hash32_t FNV1a_32(const std::string& pString);
+
+}
---
+++ src/engine/hash.cpp
@@ -0,0 +1,33 @@
+#include <engine/hash.hpp>
+
+#include <cstring>
+
+namespace hash
+{
+
+hash32_t FNV1a_32(const std::uint8_t* pData, std::size_t pSize)
+{
+	const hash32_t basis = 0x811C9DC5;
+	const hash32_t prime = 0x01000193;
+	hash32_t h = basis;
+	for (std::size_t i = 0; i < pSize; i++)
+	{
+		h ^= pData[i];
+		h *= prime;
+	}
+	return h;
+}
+
+hash32_t FNV1a_32(const char* pData, int pLength)
+{
+	if (pLength < 0)
+		pLength = std::strlen(pData);
+	return FNV1a_32((const std::uint8_t*)pData, static_cast<std::size_t>(pLength));
+}
+
+hash32_t FNV1a_32(const std::string& pString)
+{
+	return FNV1a_32(pString.c_str(), pString.size());
+}
+
+}
