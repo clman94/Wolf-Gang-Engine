cdae0c0d85105330fb79a4bf80012df9ae92ddd8
Michael Thompson <capnsword@gmail.com>
2020-07-05 15:16:40 -0700

    Removed unused

--- include/wge/scripting/script.hpp
+++ include/wge/scripting/script.hpp
@@ -25,12 +25,6 @@ public:
 
 	virtual void load() override;
 	virtual void save() override;
-
-	// Scripts are used frequently as secondary assets, so these are utility functions for creating and loading them.
-	// Creates a new script file.
-	static handle create_secondary_asset(const core::asset::ptr& pParent, const std::string& pName, const std::string& pDefault_text, const core::asset_id& pCustom_id = {});
-	// Loads an existing script file.
-	static handle load_secondary_asset(const core::asset::ptr& pParent, const std::string& pName, const core::asset_id& pId);
 };
 
 } // namespace wge::scripting
